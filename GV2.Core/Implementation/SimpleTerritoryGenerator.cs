f46b3e42808600bd01fb29a36bc6df9ec2996711
idDizel <iddizel@gmail.com>
2014-12-22 13:23:17 +0200

    Remove packages, modify gitignore

--- GV2.Core/Implementation/SimpleTerritoryGenerator.cs
+++ GV2.Core/Implementation/SimpleTerritoryGenerator.cs
@@ -11,24 +11,7 @@ namespace GV2.Core.Implementation
 
         public void GenerateCapitals(Dictionary<Structures.Point, INode> nodes, IEnumerable<IPlayer> players)
         {
-            Random rnd = new Random();
-            for (int i = 0; i < players.Count(); i++)
-            {
-                while(true)
-                {
-                    int x = rnd.Next(0, 5);
-                    int y = rnd.Next(0, 5);
-
-                    //if(hexaGrid.CanBuildCapital(new Point(){X =x, Y=y}))
-                    //{
-                    //    Players[i] = new Point() { X = x, Y = y };
-                    //    hexaGrid.Where(p => p.Point.Equals(Players[i])).FirstOrDefault().Hexagon.GetComponent<MeshRenderer>().material.color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
-                    //    he`xaGrid.Where(p => p.Point.Equals(Players[i])).FirstOrDefault().Owner = new Player() { Color = hexaGrid.Where(p => p.Point.Equals(Players[i])).FirstOrDefault().Hexagon.GetComponent<MeshRenderer>().material.color, Name = "Player" + i };
-                    //    PlayersL.Add(hexaGrid.Where(p => p.Point.Equals(Players[i])).FirstOrDefault().Owner);
-                    //    break;
-                    //}
-                }
-            }
+            throw new NotImplementedException();
         }
 
         public void GenerateTerritory(Dictionary<Structures.Point, INode> nodes, IEnumerable<IPlayer> players)
